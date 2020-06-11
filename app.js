ec51f52931ece022e7ea8acd4f172c193f780a68
blexnmirian <blexnmirian@gmail.com>
2020-06-11 15:57:58 +0100

    Added the graduates page

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const router = require('./routes/index');
+const router = require("./routes/index");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(router)
-
-
-  
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(router);
 
 const port = process.env.PORT || 8000;
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const request = require("request");
 
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-// router.get('/', (req, res) => {
-//     res.status(200).send('Welcome to Rad5 Academy Graduation page')
-// })
-
-router.get('/', (req, res) => {
-    res.render('index')
-})
-
-
-
-
-
-
-
-
-
-
-
+router.get("/graduate", (req, res) => {
+  const url = "https://rad5graduates.herokuapp.com/api";
+  res.status(200);
+  request(url, { json: true }, (err, response, body) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const data = body.classOf2020;
+      res.render("graduate", { data });
+    }
+  });
+});
 
 module.exports = router;
